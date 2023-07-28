a10c6bbaf646ec13dcdcf495684573aae42cf68d
NukeGH05T <68909193+NukeGH05T@users.noreply.github.com>
2023-07-28 23:32:24 +0600

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    sitename: 'SpringRacksTester',
+    host: 'play.simplecraft.ca',
+    port: 2963,
+    pubkey: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApA2Ah/PsZho5cCO59S91IGrAEQYCvxHAS8ktWGLy4Q7q9fktmBJ0fwAHsbt/DUKoudKZzh1LTciAHHxdZpD9wG6U1MYsvqvicIcKz/UqXsp5gnfUnNPAcS4P7XDvkVbLqV4NWNClZItWQn2Yy70K+4kecpXDUAkID/ZeeuZ6l+eWm4mQJ1RUa9GuJ/5dA4Vj7mph5/YZ9xYg0jpy9zzNpf0XkKUHX3p9TsU+OGHDYjHx0jcK+eNMr01aN6C2Sld7hdD/G62vTbcjZwG0W5XRAVtTemJnprSIAWkC+xZX0pvazQj5bms3SQY5peGLxpjiUie+3Hd7u7HK9tNasqNVkQIDAQAB'
+}
